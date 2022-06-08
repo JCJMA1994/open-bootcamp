c369432686822c06816f5bd04cf3f0f0a70c8f93
JCJMA1994 <201214026@uns.edu.pe>
2022-06-08 08:39:12 -0500

    Estructuras de control  - IF (correccion de la condicion)

--- src/com/company/EstructurasControl.java
+++ src/com/company/EstructurasControl.java
@@ -3,15 +3,20 @@ package com.company;
 public class EstructurasControl {
     public static void main(String[] args) {
         //IF
-        int numeroIF = 100;
-        if (numeroIF >= 0){
+        //Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        int numeroIF = -1;
+        if (numeroIF > 0){
             System.out.println("El numero es positivo");
-        }else{
+        }else if (numeroIF == 0){
+            System.out.println("El numero es cero");
+        }else {
             System.out.println("El numero es negativo");
         }
 
         //While
-
+        //Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+        //- Incrementar el valor de la variable en uno cada vez que se ejecute.
+        //- Mostrarlo por pantalla cada vez que se ejecute.
 
     }
 }
