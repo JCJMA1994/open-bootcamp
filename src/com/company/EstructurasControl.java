c69d751250bb2a0ba5677cef33c61c8f67a377db
JCJMA1994 <201214026@uns.edu.pe>
2022-06-08 08:35:45 -0500

    Estructuras de control  - IF

---
+++ src/com/company/EstructurasControl.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class EstructurasControl {
+    public static void main(String[] args) {
+        //IF
+        int numeroIF = 100;
+        if (numeroIF >= 0){
+            System.out.println("El numero es positivo");
+        }else{
+            System.out.println("El numero es negativo");
+        }
+
+        //While
+
+
+    }
+}
