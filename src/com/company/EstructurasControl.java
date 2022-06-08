361e1b062b002a2e93b439d0e233229cb08a1513
JCJMA1994 <201214026@uns.edu.pe>
2022-06-08 08:58:05 -0500

    Estructuras de control  - for y switch

--- src/com/company/EstructurasControl.java
+++ src/com/company/EstructurasControl.java
@@ -22,15 +22,56 @@ public class EstructurasControl {
 
         while (numberWhile < 3){
             numberWhile++;
-            System.out.println("Numero :" + numberWhile);
+            System.out.println("while numero :" + numberWhile);
         }
 
         //do...while
         //Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
         do {
             numberWhile++;
-            System.out.println("Numero: " + numberWhile);
+            System.out.println("do...while numero: " + numberWhile);
         }while (numberWhile < 3);
+
+        //for
+        //Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y
+        //su condición será que la variable sea igual o menor que 3, se irá incrementando en 1
+        //su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+
+        for (int numberFor = 0; numberFor <= 3; numberFor++){
+            System.out.println("For numero: " + numberFor);
+        }
+
+        //switch
+        //Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año.
+        //Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está.
+        //También habrá que poner un default para cuando el valor de la variable no sea una estación.
+
+        int estacion = 8;
+
+        switch (estacion){
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Estacion -> Otono");
+                break;
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Estacion -> Invierno");
+                break;
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Estacion -> Primavera");
+                break;
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Estacion -> Verano");
+                break;
+            default:
+                System.out.println("No hay estacion");
+        }
 
     }
 }
